168cd0bd25816744b9d48190f84eb5e0336adf80
Flávio Ribeiro <flavioluiz@gmail.com>
2015-03-17 00:34:46 +0100

    now use interp1 for change data rate

--- main/changedatarate.m
+++ main/changedatarate.m
@@ -6,10 +6,11 @@
 function [tnew Xnew] = changedatarate(t,X,dt)
     %modificacao da taxa de amostragem:
     tfinal = t(size(t,1));
-    ts=timeseries(X,t,'Name','speed');
-    ts=transpose(ts);
-    res_ts=resample(ts,0:dt:tfinal);
-
-    tnew = res_ts.time;
-    Xnew = reshape(res_ts.Data, size(X,2),size(tnew,1))';
+    %ts=timeseries(X,t,'Name','speed');
+    %ts=transpose(ts);
+    %res_ts=resample(ts,0:dt:tfinal);
+    tnew = 0:dt:tfinal;
+    Xnew = interp1(t,X,tnew);
+    %tnew = res_ts.time;
+    %Xnew = reshape(res_ts.Data, size(X,2),size(tnew,1))';
 end
